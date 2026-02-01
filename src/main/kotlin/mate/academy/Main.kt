844806851c24d0d6ee8517548c6695d50897caed
ivanhromovyi <ivanh@uia.no>
2026-02-01 18:55:29 +0100

    implement greetUsers() function

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,9 +1,15 @@
 package mate.academy
 
-fun greetUsers() {
-    // implement function
-}
-
 fun main() {
+    fun greetUsers() {
+        while (true) {
+            println("Please enter a name (or press Enter to quit): ")
+            val input = readLine()
+            if (input.isNullOrBlank()) {
+                break
+            }
+            println("Hello, ${input}!")
+        }
+    }
     greetUsers()
 }
